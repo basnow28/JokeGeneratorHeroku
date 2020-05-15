17bde4abb8921b01c344307660f359802885e3bb
Barbara Rekowska <basnow28@gmail.com>
2020-05-15 12:34:42 +0200

    Initial project structure

---
+++ src/test/java/joke/generator/production/ProductionApplicationTests.java
@@ -0,0 +1,13 @@
+package joke.generator.production;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
